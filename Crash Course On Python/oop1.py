7cf20dcea7e7a22cd36aaee2eab18be772054ec2
Anik Sarker Akash <56483143+aniksarakash@users.noreply.github.com>
2023-02-19 19:22:37 +0600

    1

---
+++ Crash Course On Python/oop1.py
@@ -0,0 +1,15 @@
+#Want to give this a go? Fill in the blanks in the code to make it print a poem.
+class Flower:
+  color = 'unknown'
+
+rose = Flower()
+rose.color = 'red'
+
+violet = Flower()
+violet.color = 'blue'
+
+this_pun_is_for_you = "I'm terrible at gardening, so I decided to stop and just let my plants dye."
+
+print("Roses are {},".format(rose.color))
+print("violets are {},".format(violet.color))
+print(this_pun_is_for_you)
