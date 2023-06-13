85c56ade98f5dccecd5082db23a3ff655a7ef8c4
Anik Sarker Akash <56483143+aniksarakash@users.noreply.github.com>
2023-06-13 09:10:40 +0600

    Qwiklabs week6

--- Using Python to Interact with the Operating System/week6_Creating Bash Scripts.sh
+++ Using Python to Interact with the Operating System/week6_Creating Bash Scripts.sh
@@ -11,3 +11,20 @@ echo "WHO"
 who
 echo
 echo "Finishing at: $(date)"
+
+#!/bin/bash
+echo "Starting at: $(date)"; echo
+echo "UPTIME"; uptime; echo
+1000 echo "FREE"; free; echo
+echo "WHO"; who; echo
+12 echo "Finishing at: $(date)"
+
+#Using Variables and Globs
+
+#!/bin/bash
+line="---------------------------------"
+echo "Starting at: $(date)"; echo $line
+echo "UPTIME"; uptime; echo $line
+echo "FREE"; free; echo $line
+echo "WHO"; who; echo $line
+echo "Finishing at: $(date)"
